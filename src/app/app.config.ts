72e4e8bdce7243a0ef1780babe5d70d5412b627a
LeTon25 <99533245+LeTon25@users.noreply.github.com>
2026-01-17 11:03:11 +0700

    Manage Game

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,17 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { errorInterceptor } from './interceptors/error.interceptor';
 import { provideStore } from '@ngrx/store';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor]), withFetch()),
     provideAnimations(),
-    provideStore()
+    provideStore(),
+    MessageService
 ],
 };
--- src/app/models/admin/index.ts
+++ src/app/models/admin/index.ts
@@ -1,1 +1,2 @@
 export * from './dashboard.models';
+export * from './games';
